a185b4de798a6ca7585ae77001d63dc6499b9274
Simon Schmidt <simon.schmidt@htp-tel.de>
2021-01-15 22:23:49 +0100

    add license headers

--- timer/timer.go
+++ timer/timer.go
@@ -1,4 +1,12 @@
+/*
+Copyright (C) 2021 Simon Schmidt
 
+This software is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+This software is licensed under the Creative Commons CC0 license.
+*/
 
 /* Timer implementations for RlogC */
 package timer
